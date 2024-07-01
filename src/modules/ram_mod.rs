0b3bea3ddfc95bc9175faa36f9f288c4c8b43a1a
DroidCoil <168860683+DroidCoil@users.noreply.github.com>
2024-07-01 13:56:26 -0700

    Adding change to ram_mod for machines with more than 1GB to have used mem show in MBs if under 1GB.

--- src/modules/ram_mod.rs
+++ src/modules/ram_mod.rs
@@ -11,18 +11,20 @@ pub fn ramstat() -> String {
     let used_mem = sys.used_memory() as f64 / (1024.0 * 1024.0); // Convert to MB
     let total_mem = sys.total_memory() as f64 / (1024.0 * 1024.0); // Convert to MB
 
-    if total_mem >= 1024.0 {
+    if total_mem >= 1024.0 && used_mem >= 1024.0 {
         output.push_str(&format!(
             " {:.1}GB/{:.1}GB",
-            used_mem / 1024.0, // Convert to GB
+            used_mem / 1024.0,  // Convert to GB
+            total_mem / 1024.0  // Convert to GB
+        ));
+    } else if total_mem >= 1024.0 && used_mem < 1024.0 {
+        output.push_str(&format!(
+            " {:.1}MB/{:.1}GB",
+            used_mem,
             total_mem / 1024.0 // Convert to GB
         ));
     } else {
-        output.push_str(&format!(
-            " {:.1}MB/{:.1}MB",
-            used_mem,
-            total_mem
-        ));
+        output.push_str(&format!(" {:.1}MB/{:.1}MB", used_mem, total_mem));
     }
 
     output
